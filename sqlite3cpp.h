0e2d67c099e2eb081af999a9d4174442316937bb
yangacer <yangacer@gmail.com>
2017-06-04 22:07:58 +0800

    Renew copyright year.

--- sqlite3cpp.h
+++ sqlite3cpp.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * The BSD 3-Clause License
  *
- * Copyright (c) 2016, Acer Yun-Tse Yang All rights reserved.
+ * Copyright (c) 2017, Acer Yun-Tse Yang All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
